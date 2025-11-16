cf0f6f5586f7378c4c0c2e6bb8691afe7f8055f0
Santa Tollimus <santatollimus@gmail.com>
2025-11-16 14:22:18 -0600

    Sync updated RedM resources

--- REDM/resources/[coalcounty]/coal_aim/custom_crosshair.lua
+++ REDM/resources/[coalcounty]/coal_aim/custom_crosshair.lua
@@ -1,23 +1,24 @@
 -- custom_crosshair.lua
--- Simple orange crosshair shown while aiming
+-- Compact 3-row orange crosshair shown while aiming
 
-local r, g, b = 255, 102, 0   -- color
+local r, g, b = 255, 102, 0   -- orange
 
 -- tweak these to taste
-local armSize   = 0.0022      -- bar length
+local armLength = 0.0022      -- bar length
 local thickness = 0.0006      -- bar height
-local offsetX   = 0.0019      -- left/right distance from center
+local offsetX   = 0.0020      -- left/right distance from center
 local rowGap    = 0.0020      -- vertical distance between rows
+
+local INPUT_AIM = 0xF84FA74F  -- right mouse / LT
 
 CreateThread(function()
     while true do
         Wait(0)
 
         -- Only draw when aiming
-        if IsAimCamActive() or IsControlPressed(0, 0xF84FA74F) then
+        if IsAimCamActive() or IsControlPressed(0, INPUT_AIM) then
             local cx, cy = 0.5, 0.5
 
-            -- precomputed positions
             local leftX  = cx - offsetX
             local rightX = cx + offsetX
 
@@ -26,16 +27,16 @@ CreateThread(function()
             local botY   = cy + rowGap
 
             -- MIDDLE row
-            DrawRect(leftX,  midY, armSize, thickness, r, g, b, 255)
-            DrawRect(rightX, midY, armSize, thickness, r, g, b, 255)
+            DrawRect(leftX,  midY, armLength, thickness, r, g, b, 255)
+            DrawRect(rightX, midY, armLength, thickness, r, g, b, 255)
 
             -- TOP row
-            DrawRect(leftX,  topY, armSize, thickness, r, g, b, 255)
-            DrawRect(rightX, topY, armSize, thickness, r, g, b, 255)
+            DrawRect(leftX,  topY, armLength, thickness, r, g, b, 255)
+            DrawRect(rightX, topY, armLength, thickness, r, g, b, 255)
 
             -- BOTTOM row
-            DrawRect(leftX,  botY, armSize, thickness, r, g, b, 255)
-            DrawRect(rightX, botY, armSize, thickness, r, g, b, 255)
+            DrawRect(leftX,  botY, armLength, thickness, r, g, b, 255)
+            DrawRect(rightX, botY, armLength, thickness, r, g, b, 255)
         end
     end
 end)
